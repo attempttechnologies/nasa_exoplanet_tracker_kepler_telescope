b0fd2cf85281a233ee571f05aeaf2efb1427abb1
Sulejman Limaj <limaj.sulejman@gmail.com>
2021-10-02 04:34:00 +0200

    refactor code base

--- lib/business_logic/exoplanets.dart
+++ lib/business_logic/exoplanets.dart
@@ -3,23 +3,23 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:nasa_exoplanet_tracker/presentation/exoplanet.dart';
 
+const String _baseURL =
+    "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?";
+
+const String _table = "table=cumulative";
+
+const String _query =
+    "&where=koi_disposition like 'CANDIDATE' and koi_period>300 and koi_prad<2";
+
+const String _order = "&order=koi_period";
+
+const String _returnQueryFormat = "&format=json";
+
 abstract class AbstractExoplanets {
   Future<List<Widget>> all();
 }
 
 class Exoplanets implements AbstractExoplanets {
-  final String _baseURL =
-      "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?";
-
-  final String _table = "table=cumulative";
-
-  final String _query =
-      "&where=koi_disposition like 'CANDIDATE' and koi_period>300 and koi_prad<2";
-
-  final String _order = "&order=koi_period";
-
-  final String _returnQueryFormat = "&format=json";
-
   @override
   Future<List<Widget>> all() async {
     var url = Uri.parse("$_baseURL$_table$_query$_order$_returnQueryFormat");
