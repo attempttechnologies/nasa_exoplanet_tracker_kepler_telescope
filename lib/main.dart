b5f067053d23376171bc7009f513c5b23848f2fe
Sulejman Limaj <limaj.sulejman@gmail.com>
2021-10-02 04:01:10 +0200

    Changed styling of Exoplanet name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:nasa_exoplanet_tracker/presentation/ui/exoplanets_ui.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -44,109 +42,162 @@ class MyApp extends StatelessWidget {
           margin: const EdgeInsets.all(20.0),
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Text(
+                "Kepler Object of Interest Name: ${exoplanet['kepoi_name']}",
+                style: const TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("KOI Name: ${exoplanet['kepoi_name']}",
-                      style: _textStyle()),
                   Text(
                     "Planet ID: ${exoplanet['kepid']}",
+                    style: _textStyle(),
+                  ),
+                  Text(
+                    "Planet Name: ${exoplanet['kepler_name']}",
                     style: _textStyle(),
                   ),
                 ],
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("Planet Name: ${exoplanet['kepler_name']}",
-                      style: _textStyle()),
                   Text("Disposition: ${exoplanet['koi_disposition']}",
                       style: _textStyle()),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
                   Text("Disposition Score: ${exoplanet['koi_score']}",
-                      style: _textStyle()),
-                  Text("koi_fpflag_nt: ${exoplanet['koi_fpflag_nt']}",
                       style: _textStyle()),
                 ],
               ),
               const SizedBox(
                 height: 20.0,
               ),
+              Text("koi_fpflag_nt: ${exoplanet['koi_fpflag_nt']}",
+                  style: _textStyle()),
               Text("koi_fpflag_co: ${exoplanet['koi_fpflag_co']}",
                   style: _textStyle()),
               Text("koi_fpflag_ec: ${exoplanet['koi_fpflag_ec']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_period: ${exoplanet['koi_period']}",
                   style: _textStyle()),
               Text("koi_period_err1: ${exoplanet['koi_period_err1']}",
                   style: _textStyle()),
               Text("koi_period_err2: ${exoplanet['koi_period_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_time0bk: ${exoplanet['koi_time0bk']}",
                   style: _textStyle()),
               Text("koi_time0bk_err1: ${exoplanet['koi_time0bk_err1']}",
                   style: _textStyle()),
               Text("koi_time0bk_err2: ${exoplanet['koi_time0bk_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_impact_err1: ${exoplanet['koi_impact_err1']}",
                   style: _textStyle()),
               Text("koi_impact_err2: ${exoplanet['koi_impact_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_duration: ${exoplanet['koi_duration']}",
                   style: _textStyle()),
               Text("koi_duration_err1: ${exoplanet['koi_duration_err1']}",
                   style: _textStyle()),
               Text("koi_duration_err2: ${exoplanet['koi_duration_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_depth: ${exoplanet['koi_depth']}", style: _textStyle()),
               Text("koi_depth_err1: ${exoplanet['koi_depth_err1']}",
                   style: _textStyle()),
               Text("koi_depth_err2: ${exoplanet['koi_depth_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_prad: ${exoplanet['koi_prad']}", style: _textStyle()),
               Text("koi_prad_err1: ${exoplanet['koi_prad_err1']}",
                   style: _textStyle()),
               Text("koi_prad_err2: ${exoplanet['koi_prad_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_teq: ${exoplanet['koi_teq']}", style: _textStyle()),
               Text("koi_teq_err1: ${exoplanet['koi_teq_err1']}",
                   style: _textStyle()),
               Text("koi_teq_err2: ${exoplanet['koi_teq_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_insol: ${exoplanet['koi_insol']}", style: _textStyle()),
               Text("koi_insol_err1: ${exoplanet['koi_insol_err1']}",
                   style: _textStyle()),
               Text("koi_insol_err2: ${exoplanet['koi_insol_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_model_snr: ${exoplanet['koi_model_snr']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_tce_plnt_num: ${exoplanet['koi_tce_plnt_num']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_tce_delivname: ${exoplanet['koi_tce_delivname']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_steff: ${exoplanet['koi_steff']}", style: _textStyle()),
               Text("koi_steff_err1: ${exoplanet['koi_steff_err1']}",
                   style: _textStyle()),
               Text("koi_steff_err2: ${exoplanet['koi_steff_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_slogg: ${exoplanet['koi_slogg']}", style: _textStyle()),
               Text("koi_slogg_err1: ${exoplanet['koi_slogg_err1']}",
                   style: _textStyle()),
               Text("koi_slogg_err2: ${exoplanet['koi_slogg_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_srad: ${exoplanet['koi_srad']}", style: _textStyle()),
               Text("koi_srad_err1: ${exoplanet['koi_srad_err1']}",
                   style: _textStyle()),
               Text("koi_srad_err2: ${exoplanet['koi_srad_err2']}",
                   style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("ra_str: ${exoplanet['ra_str']}", style: _textStyle()),
               Text("dec_str: ${exoplanet['dec_str']}", style: _textStyle()),
+              const SizedBox(
+                height: 10.0,
+              ),
               Text("koi_kepmag: ${exoplanet['koi_kepmag']}",
                   style: _textStyle()),
               Text("koi_kepmag_err: ${exoplanet['koi_kepmag_err']}",
@@ -155,7 +206,6 @@ class MyApp extends StatelessWidget {
           ),
         ),
       );
-      print(exoplanet);
     }
     return exoplanets;
   }
@@ -190,8 +240,6 @@ class MyApp extends StatelessWidget {
                           "images/nasa-logo-web-rgb.png",
                           width: 250.0,
                         ),
-                        const Text("Kepler Satellite's Objects of Interest"),
-                        const Text("Exoplanets Data"),
                         const SizedBox(
                           height: 20.0,
                         ),
